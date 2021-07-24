6c70280a598904e9f832fb2106a07e06daa98887
murphymUrph21 <iliveirep_831@yahoo.com>
2021-07-24 13:20:28 -0700

    completed TacoCat

--- js/site.js
+++ js/site.js
@@ -1,20 +1,26 @@
-function reverseString(){
+function reverseString() {
     let userWord = document.getElementById("tacoCat").value;
     let filteredWord = userWord.replace(/[\W_]/g, '').toLowerCase();
     let index = filteredWord.length - 1;
-    let reverseWord = "";
+    let reversedWord = "";
     for (i = index; i >= 0; i--) {
-        reverseWord += filteredWord[i];
+        reversedWord += filteredWord[i];
     }
-    if(reverseWord == filteredWord){
-        let palindrome = document.getElementById("palindrome");
-        palindrome.innerText = "True";
-    }
-    else{
+    displayData(filteredWord, reversedWord);
+}   
+
+function displayData(originalWord, reversedWord){
+    if (reversedWord == originalWord) {
+        if (reversedWord == "") {
+            let palindrome = document.getElementById("palindrome");
+            palindrome.innerText = "Please Enter a Word";
+        } else {
+            let palindrome = document.getElementById("palindrome");
+            palindrome.innerText = "True";
+        }
+    } else {
         palindrome.innerText = "False";
     }
     let output = document.getElementById("tacoOutput");
-    output.innerText = reverseWord;
+    output.innerText = reversedWord;
 }
-
-
